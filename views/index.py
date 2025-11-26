d2ca0588ee58f3b9e55fa9895b62eb23c7566afb
sdasd <vpaira@gmail.com>
2025-11-26 22:14:49 +0530

    Add 5-letter word list for validating user guesses

--- views/index.py
+++ views/index.py
@@ -49,25 +49,13 @@ def IndexView(page:ft.Page, params):
 
     def btn_simple_clicked(e):
         page.go("/simple_view")
-
-
-
-
-    #btn_question1 = ft.ElevatedButton("Question1", on_click=btn_question1_clicked)
-    #btn_question2 = ft.ElevatedButton("Question2", on_click=btn_question2_clicked)
-    #btn_simple = ft.ElevatedButton("Simple View", on_click=btn_simple_clicked)
-
     appbar = CreateAppBar()
-
     def build_board(rows, cols):
         board = ft.Column()
-
         box_txt = []
-
         for _ in range(rows):
             row = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
             row_box_txt = []
-
             for _ in range(cols):
                 txt = ft.Text(
                     value="",
@@ -86,23 +74,17 @@ def IndexView(page:ft.Page, params):
                     content=txt
                 )
                 )
-
                 row_box_txt.append(txt)
 
             box_txt.append(row_box_txt)
             board.controls.append(row)
         return board,  box_txt
 
-
-
     def check_guess(e):
         nonlocal current_row, Answer
-
         if current_row >= rows or Guess.disabled:
             return
-
         guess = Guess.value.strip().upper()
-
         if len(guess) != cols or not guess.isalpha():
             message.value = "Please Enter 5 letters."
             page.update()
@@ -124,35 +106,26 @@ def IndexView(page:ft.Page, params):
         Guess.value = ""
         page.update()
 
-
-
-
     Guess = ft.TextField(label="Type here", max_length=5, width=250, on_submit=check_guess,text_size=28,autofocus=True)
 
     message = ft.Text(
         value="Guess the Word",text_align=ft.TextAlign.CENTER,size=25,font_family="font1")
     page.update()
 
-
-
-
     board,  box_txt = build_board(rows=5, cols=5)
     current_row = 0
     rows = 5
     cols = 5
 
-
-
     wordle_words = misc.ReadCSV("data/wordle_words.txt")
+    all_words  = misc.GetAllWords("data/5-letter-words.txt")
+    #print(all_words[0:10])
     word_list = [
         (row[0].strip().upper(), row[1].strip())
         for row in wordle_words
     ]
 
     Answer, hint = random.choice(word_list)
-
-
-
     print(wordle_words)
     print("Answer:",Answer)
     page.views.append(ft.View(
